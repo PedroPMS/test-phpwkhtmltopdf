7ded8f97bf374b0f8f4b3f7de5a5da1cc352185f
Pedro Paulo <p.paulo0512@gmail.com>
2020-12-30 11:15:59 -0300

    enviando o .env encriptado

--- index.php
+++ index.php
@@ -8,18 +8,11 @@ use mikehaertl\wkhtmlto\Image; //instanciando a classe de Imagem
 $binaryPdf = 'C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe'; //definindo o caminho do binário
 $binaryImage = 'C:/Program Files/wkhtmltopdf/bin/wkhtmltoimage.exe'; //definindo o caminho do binário
 
-$pdf = new Pdf('google.com'); //definindo qual a URL a ser transformada em PDF
-$image = new Image('google.com');
+$pdf = new Pdf('https://www.objetivo.br/arquivos/livros/mem_post_bras_cubas.pdf'); //definindo qual a URL a ser transformada em PDF
 
 $pdf->binary = $binaryPdf; //setando o binário
-$image->binary = $binaryImage; //setando o binário
 
-if (!$pdf->saveAs('pdfteste.pdf')) { //salvando como google.pdf e verificando erros
+if (!$pdf->saveAs('pdfTopdf.pdf')) { //salvando como google.pdf e verificando erros
     $error = $pdf->getError();
-    print($error);
-}
-
-if (!$image->saveAs('imagetest.png')) {
-    $error = $image->getError();
     print($error);
 }
